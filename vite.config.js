27afc15948a1a2fe2763c63cb7d41a57c6a7d81a
Alejandro <abalsas@saurus.com>
2025-03-31 16:17:46 +0200

    Config files modified

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/racistometro/',
+  base: 'https://barex8.github.io/barex8.github.io',
 })
