2bbe8808429f43b8aefa8dff941b1f28d4c1d1d9
Alejandro <abalsas@saurus.com>
2025-03-31 17:01:15 +0200

    Description added

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,6 +7,9 @@ export default function Header(){
         <div>
             <div className=''>
                 < h1 className='title'>Racistómetro</h1>
+                <p> Este racistometro permite calcular la intensidad de su chiste o broma antes de que vaya realizarla, permitiendole pensar dos veces si realizarla o no.
+                    Puede calcular su grado de humor manualmente, arrastrando las etiquetas que puede encontrar al final de la página o usando Gemini AI proporcionada por Google.
+                    Simplemente indique lo que desea decir y le dirá las etiquetas que pueden describir mejor gracia. </p>
             </div>
 
         </div>
