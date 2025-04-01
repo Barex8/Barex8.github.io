c3b1c4cd66bc58b758977f1469f5f0148c15e777
Alejandro <abalsas@saurus.com>
2025-04-01 09:23:38 +0200

    fixed .js doesn't found

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://barex8.github.io/Racistometro/dist',
+  // base: 'https://barex8.github.io/Racistometro/dist',
+  base: './'
 })
